12d887ba385b71cbd76e4afbafcc1072d31de6bf
Thauan Souza <thauanqs@gmail.com>
2025-03-14 13:36:33 -0300

    Criação do array para armazenar nomes

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+// Projeto de Thauan Souza para o curso de formação ONE ORACLE
+
+let amigos = [];
+
+
